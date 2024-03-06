16c106e42531d4760cd73b2838f922b0c6883abc
marmena_meli <marcoantonio.mena@mercadolibre.com.mx>
2024-03-06 12:52:07 -0600

    first commit

---
+++ PrimerApi/cmd/main/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"net/http"
+
+	"encoding/json"
+
+	"fmt"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Person struct {
+	FirstName string `json:"FirstName"`
+	LastName  string `json:"LastName"`
+}
+
+func main() {
+
+	router := chi.NewRouter()
+
+	/*handler := func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Go Web"))
+	}*/
+
+	router.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Write([]byte("Pong"))
+	})
+
+	router.Post("/greetings", func(w http.ResponseWriter, r *http.Request) {
+
+		var requestData Person
+		err := json.NewDecoder(r.Body).Decode(&requestData)
+		if err != nil {
+			http.Error(w, "Error al decodificar el JSON", http.StatusBadRequest)
+			return
+		}
+
+		FirstNameTemp := requestData.FirstName
+		LastNameTempo := requestData.LastName
+
+		responseMessage := fmt.Sprintf("Hello %s %s", FirstNameTemp, LastNameTempo)
+
+		w.Write([]byte(responseMessage))
+
+	})
+
+	http.ListenAndServe(":8080", router)
+
+}
